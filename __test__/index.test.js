df3fb912a60310273fc1e50c95c47b6b8794ea3b
Nud <nudemeth@hotmail.com>
2018-02-13 21:41:03 +0700

    Add snapshot test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,11 +1,23 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import Router from 'next/router'
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Index from '../pages/index';
 
+const mockedRouter = { prefetch: () => {} }
+Router.router = mockedRouter
+
 describe('Index page', () => {
-    it('Should shows "This is Index Page: " in Index page', () => {
+    it('Should show "This is Index Page: " in Index page', () => {
         const index = shallow(<Index />);
         expect(index.find('h1').text()).toEqual('This is Index Page: ');
+    });
+});
+
+describe('Index page with Snapshot Testing', () => {
+    it('Should show "This is Index Page: " in Index page', () => {
+        const component = renderer.create(<Index />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
