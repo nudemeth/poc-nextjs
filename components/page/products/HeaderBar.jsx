5973b4c900335b455f5494abccfe1aec0578cbb3
Nud <nudemeth@hotmail.com>
2018-03-15 21:39:10 +0700

    Add search box to header products  page

--- components/page/products/HeaderBar.jsx
+++ components/page/products/HeaderBar.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Input, { InputAdornment  } from 'material-ui/Input';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import Icon from 'material-ui/Icon';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+    wrapper: {
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    textField: {
+        border: '1px solid white',
+        color: 'white',
+        paddingLeft: '5px'
+    }
+});
 
 class HeaderBar extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    /*static propTypes = {
+    static propTypes = {
         classes: PropTypes.object.isRequired,
         theme: PropTypes.object.isRequired
-    }*/
+    }
 
     render() {
+        const { classes } = this.props;
         return (
-            <span>This is AppBar</span>
+            <div className={classes.wrapper}>
+                <Input
+                    id="search"
+                    type="search"
+                    className={classes.textField}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <Icon>search</Icon>
+                        </InputAdornment>
+                    }
+                />
+            </div>
         );
     }
 }
 
-export default HeaderBar
+export default withStyles(styles, { withTheme: true })(HeaderBar);
