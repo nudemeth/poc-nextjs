e1c3d8bb5dc0b14c44822686ec39356bf7ed42a7
Nud <nudemeth@hotmail.com>
2018-03-18 16:40:21 +0700

    Adjust criteria textfields' width

--- pages/products.jsx
+++ pages/products.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import fetch from 'isomorphic-unfetch';
 import Container from '../components/layout/Container';
 import ProductList from '../components/page/products/ProductList';
-import HeaderBar from '../components/page/products/HeaderBar';
+import Criteria from '../components/page/products/Criteria';
 import { loadProducts } from '../actions';
 import { withReduxSaga } from '../store';
 
@@ -21,9 +21,10 @@ class Products extends React.Component {
     }
 
     render() {
+        const headerText = <span>{this.props.text}</span>
         return (
-            <Container title='Products' header={<HeaderBar/>}>
-                <h1>Products Page: {this.props.text}</h1>
+            <Container title='Products' header={headerText}>
+                <Criteria/>
                 <ProductList/>
             </Container>
         );
