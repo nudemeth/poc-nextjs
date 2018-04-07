2489f3e64a8c9a2cad549dc45f627ed12c02ffad
Nud <nudemeth@hotmail.com>
2018-04-07 20:35:25 +0700

    Isolate category item component

---
+++ components/page/category/CategoryItem.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Icon from 'material-ui/Icon';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
+const defaultColor = theme => theme.palette.grey[500];
+const selectedColor = theme => theme.palette.secondary.main;
+const categoryStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    backgroundColor: '#fff'
+}
+
+const styles = theme => ({
+    iconButtonRoot: {
+        width: '100%',
+        height: '100%'
+    },
+    iconButtonLabel: {
+        width: '100%',
+        height: 80,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly'
+    },
+    icon: {
+        color: defaultColor(theme)
+    },
+    selectedIcon: {
+        color: selectedColor(theme)
+    },
+    categoryTile: {
+        ...categoryStyle,
+        borderColor: defaultColor(theme)
+    },
+    selectedCategoryTile: {
+        ...categoryStyle,
+        borderColor: selectedColor(theme)
+    },
+    categoryRoot: {
+        height: 184,
+        padding: 2,
+        [theme.breakpoints.up('lg')]: {
+            width: '33.3333%'
+        },
+        [theme.breakpoints.down('md')]: {
+            width: '50%'
+        },
+        [theme.breakpoints.down('xs')]: {
+            width: '100%'
+        }
+    }
+});
+
+class CategoryItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected: this.props.category.isSelected
+        }
+    }
+
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+        theme: PropTypes.object.isRequired
+    }
+
+    handleCategoryToggle = () => {
+        this.setState({ isSelected: !this.state.isSelected });
+    }
+
+    render() {
+        const { classes, category } = this.props;
+        const iconClass = this.state.isSelected ? classes.selectedIcon : classes.icon;
+        const categoryTileClass = this.state.isSelected ? classes.selectedCategoryTile : classes.categoryTile;
+        return (
+            <GridListTile classes={{root: classes.categoryRoot, tile: categoryTileClass}}>
+                <IconButton disableRipple classes={{root: classes.iconButtonRoot, label: classes.iconButtonLabel}} onClick={this.handleCategoryToggle}>
+                    <Icon className={iconClass} >{category.image}</Icon>
+                    <Typography component="span">{category.name}</Typography>
+                </IconButton>
+            </GridListTile>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(CategoryItem);
--- components/page/category/CategoryList.jsx
+++ components/page/category/CategoryList.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
-import Icon from 'material-ui/Icon';
-import IconButton from 'material-ui/IconButton';
+import GridList from 'material-ui/GridList';
 import Hidden from 'material-ui/Hidden';
-import Typography from 'material-ui/Typography';
 import uuidv4 from 'uuid/v4';
 import { withStyles } from 'material-ui/styles';
 import { connect } from 'react-redux';
 import { loadProducts } from '../../../actions';
+import CategoryItem from './CategoryItem';
 
 const styles = theme => ({
     gridList: {
@@ -18,41 +16,8 @@ const styles = theme => ({
             paddingLeft: '15%',
             paddingRight: '15%'
         }
-    },
-    iconButtonRoot: {
-        width: '100%',
-        height: '100%'
-    },
-    iconButtonLabel: {
-        width: '100%',
-        height: 80,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly'
-    },
-    icon: {
-        color: theme.palette.secondary.main
-    },
-    category: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: '1px',
-        borderStyle: 'solid',
-        borderColor: theme.palette.secondary.main
     }
 });
-
-const createCategoryItem = (category, classes) => {
-    return (
-        <GridListTile key={category.id} classes={{tile: classes.category}}>
-            <IconButton disableRipple classes={{root: classes.iconButtonRoot, label: classes.iconButtonLabel}}>
-                <Icon className={classes.icon}>{category.image}</Icon>
-                <Typography component="span">{category.name}</Typography>
-            </IconButton>
-        </GridListTile>
-    );
-}
 
 class CategoryList extends React.Component {
     constructor(props) {
@@ -66,20 +31,20 @@ class CategoryList extends React.Component {
 
     render() {
         const { classes, categories } = this.props;
-        const items = categories.map((category, index) => createCategoryItem(category, classes));
+        const items = categories.map((category, index) => <CategoryItem key={uuidv4()} category={category} />);
         return [
             <Hidden mdDown key={uuidv4()}>
-                <GridList cellHeight={180} cols={3} className={classes.gridList}>
+                <GridList className={classes.gridList}>
                     {items}
                 </GridList>
             </Hidden>
             ,<Hidden xsDown lgUp key={uuidv4()}>
-                <GridList cellHeight={180} cols={2} className={classes.gridList}>
+                <GridList className={classes.gridList}>
                     {items}
                 </GridList>
             </Hidden>
             ,<Hidden smUp key={uuidv4()}>
-                <GridList cellHeight={180} cols={1} className={classes.gridList}>
+                <GridList className={classes.gridList}>
                     {items}
                 </GridList>
             </Hidden>
