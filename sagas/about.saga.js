21fbb8046817c1e27ea1bd2a0c9e91b1dd9c3495
nudemeth <nudemeth>
2018-06-25 21:39:28 +0700

    Refactor saga effects

--- sagas/about.saga.js
+++ sagas/about.saga.js
@@ -1,11 +1,10 @@
-import { delay } from 'redux-saga';
-import { all, call, put, take, takeLatest, takeEvery, select } from 'redux-saga/effects';
+import * as effects from 'redux-saga/effects';
 import actionTypes from '../actions/actionTypes';
 import * as actions from '../actions/about.actions';
 
 function * updateGreetingSaga() {
-    yield take(actionTypes.UPDATE);
-    yield put(actions.updateGreeting('This is from client'));
+    yield effects.take(actionTypes.UPDATE);
+    yield effects.put(actions.updateGreeting('This is from client'));
 }
 
 export { updateGreetingSaga };
--- sagas/category.saga.js
+++ sagas/category.saga.js
@@ -1,36 +1,33 @@
-import { delay } from 'redux-saga';
-import { all, call, put, take, takeLatest, takeEvery, select } from 'redux-saga/effects';
-import fetch from 'isomorphic-unfetch';
-import actionTypes from '../actions/actionTypes';
+import * as effects from 'redux-saga/effects';
 import * as actions from '../actions/category.actions';
 import categoryApi from '../api/category.api';
 
 function * loadCategoriesWorker() {
-    const { categoryReducer: { categories } } = yield select();
+    const { categoryReducer: { categories } } = yield effects.select();
     if (!categories || categories.length === 0) {
         try {
-            const res = yield call(categoryApi.getCategories);
+            const res = yield effects.call(categoryApi.getCategories);
             const data = yield res.json();
-            yield put(actions.loadCategoriesSuccess(data));
+            yield effects.put(actions.loadCategoriesSuccess(data));
         } catch(err) {
-            yield put(actions.loadCategoriesFailure(err));
+            yield effects.put(actions.loadCategoriesFailure(err));
         }
     }
 }
 
 function * getSelectedCategoryIds() {
-    yield call(loadCategoriesWorker);
-    const { categoryReducer: { categories } } = yield select();
+    yield effects.call(loadCategoriesWorker);
+    const { categoryReducer: { categories } } = yield effects.select();
     return categories.filter((category) => category.isSelected).map((category) => category.id);
 }
 
 function * updateSelectedCategory(action) {
     const { category, isSelected } = action.payload;
-    const { categoryReducer: { categories } } = yield select();
+    const { categoryReducer: { categories } } = yield effects.select();
     category.isSelected = isSelected;
     const idx = categories.findIndex(c => c.id === category.id);
     categories[idx] = category;
-    yield put(actions.loadCategoriesSuccess(categories));
+    yield effects.put(actions.loadCategoriesSuccess(categories));
 }
 
 export { getSelectedCategoryIds };
