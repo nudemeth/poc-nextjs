a7cf9aa657bcc29f0a896aa298228251b51a782a
nudemeth <nudemeth>
2018-06-25 21:51:24 +0700

    Refactor saga effects

--- sagas/product.saga.js
+++ sagas/product.saga.js
@@ -1,29 +1,26 @@
-import { delay } from 'redux-saga';
-import { all, call, put, take, takeLatest, takeEvery, select } from 'redux-saga/effects';
-import fetch from 'isomorphic-unfetch';
-import actionTypes from '../actions/actionTypes';
+import * as effects from 'redux-saga/effects';
 import * as actions from '../actions/product.actions';
 import productApi from '../api/product.api';
 import * as categorySaga from './category.saga';
 
 function * loadProductsWorker() {
     try {
-        const categories = yield call(categorySaga.getSelectedCategoryIds);
-        const res = yield call(productApi.getProducts, categories);
+        const categories = yield effects.call(categorySaga.getSelectedCategoryIds);
+        const res = yield effects.call(productApi.getProducts, categories);
         const data = yield res.json();
-        yield put(actions.loadProductsSuccess(data));
+        yield effects.put(actions.loadProductsSuccess(data));
     } catch(err) {
-        yield put(actions.loadProductsFailure(err));
+        yield effects.put(actions.loadProductsFailure(err));
     }
 }
 
 function * loadProductWorker(action) {
     try {
-        const res = yield call(productApi.getProduct, action.payload.id);
+        const res = yield effects.call(productApi.getProduct, action.payload.id);
         const data = yield res.json();
-        yield put(actions.loadProductSuccess(data));
+        yield effects.put(actions.loadProductSuccess(data));
     } catch(err) {
-        yield put(actions.loadProductFailure(err));
+        yield effects.put(actions.loadProductFailure(err));
     }
 }
 
--- sagas/root.saga.js
+++ sagas/root.saga.js
@@ -1,17 +1,16 @@
-import { delay } from 'redux-saga';
-import { all, call, put, take, takeLatest, takeEvery, select } from 'redux-saga/effects';
+import * as effects from 'redux-saga/effects';
 import actionTypes from '../actions/actionTypes';
 import * as productSaga from './product.saga';
 import * as categorySaga from './category.saga';
 import * as aboutSaga from './about.saga';
 
 function * rootWatcher() {
-    yield all([
-        call(aboutSaga.updateGreetingSaga),
-        takeEvery(actionTypes.LOAD_PRODUCTS, productSaga.loadProductsWorker),
-        takeEvery(actionTypes.LOAD_PRODUCT, productSaga.loadProductWorker),
-        takeEvery(actionTypes.LOAD_CATEGORIES, categorySaga.loadCategoriesWorker),
-        takeEvery(actionTypes.UPDATE_SELECTED_CATEGORY, categorySaga.updateSelectedCategory)
+    yield effects.all([
+        effects.call(aboutSaga.updateGreetingSaga),
+        effects.takeEvery(actionTypes.LOAD_PRODUCTS, productSaga.loadProductsWorker),
+        effects.takeEvery(actionTypes.LOAD_PRODUCT, productSaga.loadProductWorker),
+        effects.takeEvery(actionTypes.LOAD_CATEGORIES, categorySaga.loadCategoriesWorker),
+        effects.takeEvery(actionTypes.UPDATE_SELECTED_CATEGORY, categorySaga.updateSelectedCategory)
     ]);
 }
 
