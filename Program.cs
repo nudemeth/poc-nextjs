92700a25b3874598b4c768f386fe85d0df6599a9
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-07-07 14:38:23 +0700

    Change assembly name to Catalog.API

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace poc_dotnetcore
+namespace Catalog.API
 {
     public class Program
     {
