01c8f8e985944342808a217ecf1aff243bf54273
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-08-11 16:25:54 +0700

    Update fake api for web-nextjs

---
+++ web-nextjs/fake/api-gateway/server.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+const routes = require('./routes')
+
+server.use(middlewares)
+server.get('/api/v1/catalog/items/:id/img', (req, res) => {
+    const id = req.params.id
+    const imgPath = path.join(__dirname, 'images', `Item ${id}.png`)
+    res.sendFile(imgPath)
+})
+server.use(jsonServer.rewriter(routes))
+server.use(router)
+server.listen(5000, "0.0.0.0", () => {
+    console.log('Fake api gateway server is running')
+})
