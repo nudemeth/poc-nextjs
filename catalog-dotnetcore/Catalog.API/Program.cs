1da7dafa36ea4057484a9031157cd96f8e0353f6
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-08-20 20:56:33 +0700

    Add cert to catalog api

--- catalog-dotnetcore/Catalog.API/Program.cs
+++ catalog-dotnetcore/Catalog.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Autofac.Extensions.DependencyInjection;
+using System.Net;
 
 namespace Catalog.API
 {
@@ -21,6 +22,13 @@ namespace Catalog.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureServices(s => s.AddAutofac())
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel(o =>
+                {
+                    o.Listen(IPAddress.Loopback, 5000, lo =>
+                    {
+                        lo.UseHttps("./cert.pfx", "7ANZNx^BVd12");
+                    });
+                });
     }
 }
