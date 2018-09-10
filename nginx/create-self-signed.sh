0eef53436d55a224f0c9f216c41788601916bc6c
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-09-10 21:26:49 +0700

    Update cert valid period to 3650 days

--- nginx/create-self-signed.sh
+++ nginx/create-self-signed.sh
@@ -2,7 +2,7 @@ org=localhost
 domain=localhost
 
 openssl genpkey -algorithm RSA -out "/data/$domain".key
-openssl req -x509 -key "/data/$domain".key -out "/data/$domain".crt \
+openssl req -x509 -key "/data/$domain".key -out "/data/$domain".crt -days 3650 \
     -subj "/CN=*.$domain/O=$org" \
     -config "/data/openssl.cnf" \
     -extensions x509_ext
