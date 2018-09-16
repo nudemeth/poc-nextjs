e585b8c0425c51a88bd9136ab7154f826f176123
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-09-16 14:29:36 +0700

    api-go: Use env var to set binding port for

--- api-go/src/api-go/main.go
+++ api-go/src/api-go/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"api-go/handlers"
 	"log"
 	"net/http"
-
-	"api-go/handlers"
+	"os"
 )
 
 func main() {
 	log.Print("Starting the server")
 
 	router := handlers.Router()
+	port := os.Getenv("SERVICE_PORT")
+
+	if port == "" {
+		port = "8000"
+		log.Printf("SERVICE_PORT is not set in environment. Default to: %s", port)
+	}
 
 	log.Print("The server is ready to listen and serve")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
