436891cf7b803f707373d60652e97ec5e2e0829c
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-09-16 15:22:17 +0700

    api-go: Implement gracefully shutdown

--- api-go/src/api-go/main.go
+++ api-go/src/api-go/main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
-	"api-go/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"api-go/handlers"
 )
 
 func main() {
 	log.Print("Starting the server")
 
-	router := handlers.Router()
 	port := os.Getenv("SERVICE_PORT")
 
 	if port == "" {
 		port = "8000"
-		log.Printf("SERVICE_PORT is not set in environment. Default to: %s", port)
+		log.Printf("SERVICE_PORT is not set in environment variable. Default to: %s", port)
 	}
 
-	log.Print("The server is ready to listen and serve")
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	router := handlers.Router()
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	log.Print("The service is ready to listen and serve...")
+
+	killSignal := <-stop
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Get SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("The service has been gracefully shut down.")
 }
