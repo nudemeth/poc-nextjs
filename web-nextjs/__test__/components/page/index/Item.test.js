8979a9d7b9cfe4af756b3e46fa7443d60b18f6ad
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-10-06 13:33:18 +0700

    web-nextjs: Update protocal for dev

--- web-nextjs/__test__/components/page/index/Item.test.js
+++ web-nextjs/__test__/components/page/index/Item.test.js
@@ -12,7 +12,7 @@ const item = {
 describe('Item component', () => {
     it('Should show Item component', () => {
         const wrapper = shallow(<Item classes={classes} theme={theme} item={item} />)
-        expect(wrapper.find('img').prop('src')).toEqual(`https://localhost:5000/api/v1/catalog/items/${item.id}/img`)
+        expect(wrapper.find('img').prop('src')).toEqual(`http://localhost:5000/api/v1/catalog/items/${item.id}/img`)
         expect(wrapper.find('img').prop('alt')).toEqual('Item Name')
         expect(wrapper.find('WithStyles(GridListTileBar)').prop('title')).toEqual('Item Name')
     })
