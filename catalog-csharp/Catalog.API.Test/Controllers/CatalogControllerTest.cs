d60fcf51da0ea1ee0dfd79c754e1b27261a2e0ba
nudemeth <7686682+nudemeth@users.noreply.github.com>
2018-10-22 21:16:57 +0700

    catalog-csharp: Update test project

--- catalog-csharp/Catalog.API.Test/Controllers/CatalogControllerTest.cs
+++ catalog-csharp/Catalog.API.Test/Controllers/CatalogControllerTest.cs
@@ -15,44 +15,43 @@ namespace ServiceTest.Controller
     {
         private CatalogController controller;
         private Mock<ICatalogService> mockCatalogService;
-        private IList<CatalogItem> mockItems = new List<CatalogItem>()
-        {
-            new CatalogItem() { Id = 1, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Bot Black Sweatshirt", Name = ".NET Bot Black Sweatshirt", Price = 19.5M, FileName = "Item 1.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 2, CatalogTypeId = 1, CatalogBrandId = 2, Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, FileName = "Item 2.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 3, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, FileName = "Item 3.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 4, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Foundation Sweatshirt", Name = ".NET Foundation Sweatshirt", Price = 12, FileName = "Item 4.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 5, CatalogTypeId = 3, CatalogBrandId = 5, Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, FileName = "Item 5.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 6, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Blue Sweatshirt", Name = ".NET Blue Sweatshirt", Price = 12, FileName = "Item 6.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 7, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, FileName = "Item 7.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 8, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Kudu Purple Sweatshirt", Name = "Kudu Purple Sweatshirt", Price = 8.5M, FileName = "Item 8.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 9, CatalogTypeId = 1, CatalogBrandId = 5, Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, FileName = "Item 9.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 10, CatalogTypeId = 3, CatalogBrandId = 2, Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, FileName = "Item 10.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 11, CatalogTypeId = 3, CatalogBrandId = 2, Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, FileName = "Item 11.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 12, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, FileName = "Item 12.png", CreatedDate = DateTime.Today }
-        };
-        private IList<CatalogType> mockTypes = new List<CatalogType>()
-        {
-            new CatalogType() { Id = 1, Type = "Mug", Icon = "local_cafe", IsSelected = true },
-            new CatalogType() { Id = 2, Type = "T-Shirt", Icon = "person", IsSelected = true },
-            new CatalogType() { Id = 3, Type = "Sheet", Icon = "label", IsSelected = true },
-            new CatalogType() { Id = 4, Type = "USB Memory Stick", Icon = "usb", IsSelected = true }
-        };
-
-        private IList<CatalogBrand> mockBrands = new List<CatalogBrand>()
-        {
-            new CatalogBrand() { Id = 1, Brand = "Azure", Icon = "cloud", IsSelected = true },
-            new CatalogBrand() { Id = 2, Brand = ".NET", Icon = "code", IsSelected = true },
-            new CatalogBrand() { Id = 3, Brand = "Visual Studio", Icon = "polymer", IsSelected = true },
-            new CatalogBrand() { Id = 4, Brand = "SQL Server", Icon = "storage", IsSelected = true },
-            new CatalogBrand() { Id = 5, Brand = "Other", Icon = "category", IsSelected = true }
-        };
+        private IList<CatalogItem> mockItems = new List<CatalogItem>();
+        private IList<CatalogType> mockTypes = new List<CatalogType>();
+        private IList<CatalogBrand> mockBrands = new List<CatalogBrand>();
 
         public CatalogControllerTest()
         {
+            SetUpMockData();
             SetUpMock();
             controller = new CatalogController(mockCatalogService.Object);
         }
 
+        private void SetUpMockData()
+        {
+            mockTypes.Add(new CatalogType() { Id = Guid.NewGuid(), Type = "Mug", Icon = "local_cafe", IsSelected = true });
+            mockTypes.Add(new CatalogType() { Id = Guid.NewGuid(), Type = "T-Shirt", Icon = "person", IsSelected = true });
+            mockTypes.Add(new CatalogType() { Id = Guid.NewGuid(), Type = "Sheet", Icon = "label", IsSelected = true });
+            mockTypes.Add(new CatalogType() { Id = Guid.NewGuid(), Type = "USB Memory Stick", Icon = "usb", IsSelected = true });
+
+            mockBrands.Add(new CatalogBrand() { Id = Guid.NewGuid(), Brand = "Azure", Icon = "cloud", IsSelected = true });
+            mockBrands.Add(new CatalogBrand() { Id = Guid.NewGuid(), Brand = ".NET", Icon = "code", IsSelected = true });
+            mockBrands.Add(new CatalogBrand() { Id = Guid.NewGuid(), Brand = "Visual Studio", Icon = "polymer", IsSelected = true });
+            mockBrands.Add(new CatalogBrand() { Id = Guid.NewGuid(), Brand = "SQL Server", Icon = "storage", IsSelected = true });
+            mockBrands.Add(new CatalogBrand() { Id = Guid.NewGuid(), Brand = "Other", Icon = "category", IsSelected = true });
+
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = ".NET Bot Black Sweatshirt", Name = ".NET Bot Black Sweatshirt", Price = 19.5M, FileName = "Item 1.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "Mug").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, FileName = "Item 2.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, FileName = "Item 3.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = ".NET Foundation Sweatshirt", Name = ".NET Foundation Sweatshirt", Price = 12, FileName = "Item 4.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "Sheet").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, FileName = "Item 5.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = ".NET Blue Sweatshirt", Name = ".NET Blue Sweatshirt", Price = 12, FileName = "Item 6.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, FileName = "Item 7.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Kudu Purple Sweatshirt", Name = "Kudu Purple Sweatshirt", Price = 8.5M, FileName = "Item 8.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "Mug").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, FileName = "Item 9.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "Sheet").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, FileName = "Item 10.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "Sheet").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == ".NET").Select(c => c.Id).First(), Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, FileName = "Item 11.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = mockTypes.Where(c => c.Type == "T-Shirt").Select(c => c.Id).First(), CatalogBrandId = mockBrands.Where(c => c.Brand == "Other").Select(c => c.Id).First(), Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, FileName = "Item 12.png", CreatedDate = DateTime.Today });
+        }
         private void SetUpMock()
         {
             var mock = new Mock<ICatalogService>();
@@ -72,12 +71,6 @@ namespace ServiceTest.Controller
         public void WhenCallGetItemsByIds_ShouldExist()
         {
             AssertMethodExists("GetItemsByIds");
-        }
-
-        [Fact]
-        public void WhenCallGetItemById_ShouldExist()
-        {
-            AssertMethodExists("GetItemById");
         }
 
         [Fact]
@@ -127,7 +120,7 @@ namespace ServiceTest.Controller
         [Fact]
         public async void WhenCallGetItemsByIds_ShouldGetItemsFilteredByIds()
         {
-            var ids = new int[] { 1, 2, 3 };
+            var ids = mockItems.Select(c => c.Id).ToArray();
             var result = await controller.GetItemsByIds(String.Join(',', ids)) as OkObjectResult;
             var expected = mockItems.Where(i => ids.Contains(i.Id)).ToList();
             AssertResultOk(result);
@@ -137,8 +130,8 @@ namespace ServiceTest.Controller
         [Fact]
         public async void WhenCallGetItemsById_ShouldGetItemFilteredById()
         {
-            var id = 3;
-            var result = await controller.GetItemById(id) as OkObjectResult;
+            var id = mockItems.First().Id;
+            var result = await controller.GetItemsByIds(id.ToString()) as OkObjectResult;
             var expected = mockItems.Where(i => i.Id == id).ToList();
             AssertResultOk(result);
             Assert.Equal(expected, result.Value);
@@ -157,7 +150,7 @@ namespace ServiceTest.Controller
         [Fact]
         public async void WhenCallGetItemsByTypes_ShouldGetItemFilteredByTypes()
         {
-            var ids = new int[] { 1, 3, 4 };
+            var ids = mockTypes.Select(c => c.Id).Take(3);
             var result = await controller.GetItemsByTypes(String.Join(',', ids)) as OkObjectResult;
             var expected = mockItems.Where(i => ids.Contains(i.CatalogTypeId)).ToList();
             AssertResultOk(result);
@@ -167,7 +160,7 @@ namespace ServiceTest.Controller
         [Fact]
         public async void WhenCallGetItemsByBrands_ShouldGetItemFilteredByTypes()
         {
-            var ids = new int[] { 1, 3, 4 };
+            var ids = mockBrands.Select(c => c.Id).Take(3);
             var result = await controller.GetItemsByBrands(String.Join(',', ids)) as OkObjectResult;
             var expected = mockItems.Where(i => ids.Contains(i.CatalogBrandId)).ToList();
             AssertResultOk(result);
@@ -177,8 +170,8 @@ namespace ServiceTest.Controller
         [Fact]
         public async void WhenCallGetItemsByTypesAndBrands_ShouldGetItemFilteredByTypesAndBrands()
         {
-            var typeIds = new int[] { 1, 2 };
-            var brandIds = new int[] { 2, 3 };
+            var typeIds = mockTypes.Select(c => c.Id).Take(2);
+            var brandIds = mockBrands.Select(c => c.Id).Take(2);
             var result = await controller.GetItemsByTypesAndBrands(String.Join(',', typeIds), String.Join(',', brandIds)) as OkObjectResult;
             var expected = mockItems.Where(i => typeIds.Contains(i.CatalogTypeId) && brandIds.Contains(i.CatalogBrandId)).ToList();
             AssertResultOk(result);
--- catalog-csharp/Catalog.API.Test/Controllers/ImageControllerTest.cs
+++ catalog-csharp/Catalog.API.Test/Controllers/ImageControllerTest.cs
@@ -17,24 +17,11 @@ namespace Catalog.API.Test.Controller
         private ImageController controller;
         private IDictionary<string, string> config;
         private Mock<ICatalogService> mockCatalogService;
-        private IList<CatalogItem> mockItems = new List<CatalogItem>()
-        {
-            new CatalogItem() { Id = 1, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Bot Black Sweatshirt", Name = ".NET Bot Black Sweatshirt", Price = 19.5M, FileName = "Item 1.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 2, CatalogTypeId = 1, CatalogBrandId = 2, Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, FileName = "Item 2.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 3, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, FileName = "Item 3.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 4, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Foundation Sweatshirt", Name = ".NET Foundation Sweatshirt", Price = 12, FileName = "Item 4.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 5, CatalogTypeId = 3, CatalogBrandId = 5, Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, FileName = "Item 5.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 6, CatalogTypeId = 2, CatalogBrandId = 2, Description = ".NET Blue Sweatshirt", Name = ".NET Blue Sweatshirt", Price = 12, FileName = "Item 6.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 7, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, FileName = "Item 7.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 8, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Kudu Purple Sweatshirt", Name = "Kudu Purple Sweatshirt", Price = 8.5M, FileName = "Item 8.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 9, CatalogTypeId = 1, CatalogBrandId = 5, Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, FileName = "Item 9.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 10, CatalogTypeId = 3, CatalogBrandId = 2, Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, FileName = "Item 10.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 11, CatalogTypeId = 3, CatalogBrandId = 2, Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, FileName = "Item 11.png", CreatedDate = DateTime.Today },
-            new CatalogItem() { Id = 12, CatalogTypeId = 2, CatalogBrandId = 5, Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, FileName = "Item 12.png", CreatedDate = DateTime.Today }
-        };
+        private IList<CatalogItem> mockItems = new List<CatalogItem>();
 
         public ImageControllerTest()
         {
+            SetUpMockData();
             SetUpMock();
             config = new Dictionary<string, string>()
             {
@@ -43,6 +30,21 @@ namespace Catalog.API.Test.Controller
             controller = new ImageController(mockCatalogService.Object, config);
         }
 
+        private void SetUpMockData()
+        {
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = ".NET Bot Black Sweatshirt", Name = ".NET Bot Black Sweatshirt", Price = 19.5M, FileName = "Item 1.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = ".NET Black & White Mug", Name = ".NET Black & White Mug", Price= 8.50M, FileName = "Item 2.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Prism White T-Shirt", Name = "Prism White T-Shirt", Price = 12, FileName = "Item 3.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = ".NET Foundation Sweatshirt", Name = ".NET Foundation Sweatshirt", Price = 12, FileName = "Item 4.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Roslyn Red Sheet", Name = "Roslyn Red Sheet", Price = 8.5M, FileName = "Item 5.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = ".NET Blue Sweatshirt", Name = ".NET Blue Sweatshirt", Price = 12, FileName = "Item 6.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Roslyn Red T-Shirt", Name = "Roslyn Red T-Shirt", Price = 12, FileName = "Item 7.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Kudu Purple Sweatshirt", Name = "Kudu Purple Sweatshirt", Price = 8.5M, FileName = "Item 8.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Cup<T> White Mug", Name = "Cup<T> White Mug", Price = 12, FileName = "Item 9.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = ".NET Foundation Sheet", Name = ".NET Foundation Sheet", Price = 12, FileName = "Item 10.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Cup<T> Sheet", Name = "Cup<T> Sheet", Price = 8.5M, FileName = "Item 11.png", CreatedDate = DateTime.Today });
+            mockItems.Add(new CatalogItem() { Id = Guid.NewGuid(), CatalogTypeId = Guid.NewGuid(), CatalogBrandId = Guid.NewGuid(), Description = "Prism White TShirt", Name = "Prism White TShirt", Price = 12, FileName = "Item 12.png", CreatedDate = DateTime.Today });
+        }
         private void SetUpMock()
         {
             var mock = new Mock<ICatalogService>();
@@ -60,12 +62,12 @@ namespace Catalog.API.Test.Controller
         [Fact]
         public async void WhenGetImage_ShouldGetImageFileFromId()
         {
-            var id = 3;
+            var id = mockItems.First().Id.ToString();
             var result = await controller.GetImage(id) as FileContentResult;
 
             Assert.NotNull(result);
             
-            var item = mockItems.Where(i => i.Id == id).SingleOrDefault();
+            var item = mockItems.Where(i => i.Id == new Guid(id)).SingleOrDefault();
             var path = Path.Combine(config["image-path"], item.FileName);
             var imageFileExtension = Path.GetExtension(item.FileName);
             var mimetype = "image/png";
