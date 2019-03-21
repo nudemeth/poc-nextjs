14584678eca0f7bac66dbd8486a032a934e0d01f
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-03-21 20:58:53 +0700

    api-go: Add test for identity

--- api-go/src/api-go/handlers/identity_test.go
+++ api-go/src/api-go/handlers/identity_test.go
@@ -6,23 +6,23 @@ import (
 	"testing"
 )
 
-func TestCatalogRouter(t *testing.T) {
+func TestIdentityRouter(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status":"OK"}`))
 	}))
 	defer server.Close()
 
-	handler := Config{CatalogBaseURL: server.URL}
+	handler := Config{IdentityBaseURL: server.URL}
 	r := handler.CreateRouter()
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	res, err := http.Get(ts.URL + "/api/v1/catalog")
+	res, err := http.Get(ts.URL + "/api/v1/identity")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if res.StatusCode != http.StatusOK {
-		t.Errorf("Status code for /api/v1/catalog is wrong. Actual: %d, Expected: %d.", res.StatusCode, http.StatusOK)
+		t.Errorf("Status code for /api/v1/identity is wrong. Actual: %d, Expected: %d.", res.StatusCode, http.StatusOK)
 	}
 }
