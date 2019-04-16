a3f17d0cba91e11003a68ca80e8ec12c7669a696
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-04-16 12:32:18 +0700

    identity-java: Update test class to use test resource

--- identity-java/src/test/java/nudemeth/poc/identity/GreetingControllerTests.java
+++ identity-java/src/test/java/nudemeth/poc/identity/GreetingControllerTests.java
@@ -8,8 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class GreetingControllerTests {
 
     @Autowired
