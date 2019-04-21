d7ff708e67ffd2bdd344cfe1b53d893bdb293a2b
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-04-21 13:18:22 +0700

    identity-java: Apply lombok to the project

--- identity-java/src/main/java/nudemeth/poc/identity/entity/UserEntity.java
+++ identity-java/src/main/java/nudemeth/poc/identity/entity/UserEntity.java
@@ -8,75 +8,28 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "USER")
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
     private UUID id;
+    @Getter @Setter @NonNull
     private String login;
+    @Getter @Setter @NonNull
     private String name;
+    @Getter @Setter @NonNull
     private String email;
-
-    public UserEntity(UUID id, String login, String name, String email) {
-        this(login, name, email);
-        this.id = id;
-    }
-
-    public UserEntity(String login, String name, String email) {
-        this.login = login;
-        this.name = name;
-        this.email = email;
-    }
-
-    /**
-     * @return the id
-     */
-    public UUID getId() {
-        return id;
-    }
-
-    /**
-     * @return the login
-     */
-    public String getLogin() {
-        return login;
-    }
-
-    /**
-     * @param login the login to set
-     */
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
 }
--- identity-java/src/main/java/nudemeth/poc/identity/model/UserModel.java
+++ identity-java/src/main/java/nudemeth/poc/identity/model/UserModel.java
@@ -2,71 +2,23 @@ package nudemeth.poc.identity.model;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserModel {
+    @Getter
     private UUID id;
-    private String email;
+    @Getter @Setter @NonNull
     private String login;
+    @Getter @Setter @NonNull
     private String name;
-
-    public UserModel(String login, String name, String email) {
-        this.login = login;
-        this.name = name;
-        this.email = email;
-    }
-
-    public UserModel(UUID id, String login, String name, String email) {
-        this(login, name, email);
-        this.id = id;
-    }
-
-    /**
-     * @return the id
-     */
-    public UUID getId() {
-        return id;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the login
-     */
-    public String getLogin() {
-        return login;
-    }
-
-    /**
-     * @param login the login to set
-     */
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    
+    @Getter @Setter @NonNull
+    private String email;
 }
