4d917234e73957a8bd5c397a1ad320e74a8234a5
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-04-28 13:26:50 +0700

    identity-java: Fix cannot retrieve data from db

--- identity-java/src/main/java/nudemeth/poc/identity/entity/UserEntity.java
+++ identity-java/src/main/java/nudemeth/poc/identity/entity/UserEntity.java
@@ -11,15 +11,17 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "user", schema = "public")
 @RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class UserEntity {
