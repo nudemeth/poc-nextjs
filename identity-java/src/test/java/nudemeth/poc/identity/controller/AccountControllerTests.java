f60016a1db862c9b459d292b7f622b207e069891
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-05-03 23:30:09 +0700

    identity-java: Add more tests to acct controller

--- identity-java/src/test/java/nudemeth/poc/identity/controller/AccountControllerTests.java
+++ identity-java/src/test/java/nudemeth/poc/identity/controller/AccountControllerTests.java
@@ -1,17 +1,20 @@
 package nudemeth.poc.identity.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +36,8 @@ public class AccountControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
 
     @MockBean
     private AccountService mockAccountService;
@@ -44,6 +50,39 @@ public class AccountControllerTests {
             .andExpect(status().reason("Request method 'GET' not supported"));
 
         verify(mockAccountService, never()).getUserByLogin(anyString());
+    }
+
+    @Test
+    public void getUser_WhenWithIdPathParam_ShouldReturnJsonUser() throws Exception {
+        UUID id = UUID.randomUUID();
+        String login = "testLogin";
+        String name = "Test Name";
+        String email = "Test.Email@test.com";
+        Optional<UserModel> user = Optional.of(new UserModel(id, login, name, email));
+
+        when(mockAccountService.getUser(id)).thenReturn(user);
+        
+        this.mockMvc.perform(get(String.format("/users/%s", id.toString())))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(id.toString()))
+            .andExpect(jsonPath("$.login").value(login))
+            .andExpect(jsonPath("$.name").value(name))
+            .andExpect(jsonPath("$.email").value(email));
+
+        verify(mockAccountService, atLeastOnce()).getUser(id);
+    }
+
+    @Test
+    public void getUser_WhenWithInvalidIdPathParam_ShouldReturnBadRequest() throws Exception {
+        String id = "some-invalid-uuid";
+        
+        this.mockMvc.perform(get(String.format("/users/%s", id)))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(status().reason(String.format("Invalid UUID string: %s", id)));
+
+        verify(mockAccountService, never()).getUser(any(UUID.class));
     }
 
     @Test
@@ -86,6 +125,30 @@ public class AccountControllerTests {
             .andExpect(jsonPath("$.email").value(email));
 
         verify(mockAccountService, atLeastOnce()).getUserByEmail(anyString());
+    }
+
+    @Test
+    public void createUser_WhenWithUserModel_ShouldReturnUUIDWithCreatedHttpStatus() throws Exception {
+        UUID id = UUID.randomUUID();
+        String login = "testLogin";
+        String name = "Test Name";
+        String email = "Test.Email@test.com";
+        UserModel user = new UserModel(login, name, email);
+        String jsonUser = mapper.writeValueAsString(user);
+        
+        when(mockAccountService.createUser(user)).thenReturn(id);
+        
+        this.mockMvc.perform(
+                post("/users")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUser)
+            )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(id.toString()));
+
+        verify(mockAccountService, atLeastOnce()).createUser(user);
     }
 
 }
