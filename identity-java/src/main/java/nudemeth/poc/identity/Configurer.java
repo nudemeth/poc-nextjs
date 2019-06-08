b4693213534891a85d1dbe2580517cb95a71964a
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-06-08 16:03:53 +0700

    identity-java: Add CORS support

---
+++ identity-java/src/main/java/nudemeth/poc/identity/Configurer.java
@@ -0,0 +1,42 @@
+package nudemeth.poc.identity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Configurer implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
+
+    private Logger logger = LoggerFactory.getLogger(Configurer.class);
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] origin = GetAllowedOrigin();
+        registry.addMapping("/**").allowedOrigins(origin);
+    }
+
+    private String[] GetAllowedOrigin() {
+        String url = environment.getProperty("api.url");
+        String urlTls = environment.getProperty("api.url.tls");
+        String apiUrl = System.getenv("API_BASE_URL");
+        String apiUrlTls = System.getenv("API_BASE_URL_TLS");
+
+        if (apiUrl != null) {
+            url = apiUrl;
+        }
+
+        if (apiUrlTls != null) {
+            urlTls = apiUrlTls;
+        }
+        
+        return new String[] { url, urlTls };
+    }
+}
