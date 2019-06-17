74da6950694224f0a731039ccd3719da25b11289
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-06-17 20:53:04 +0700

    api-go: Add identity tests

--- api-go/src/api-go/handlers/identity_test.go
+++ api-go/src/api-go/handlers/identity_test.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"api-go/api"
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -69,6 +71,81 @@ func TestIdentityUserInfo(t *testing.T) {
 	}
 
 	if actual, expected := string(text), `{"status":"OK"}`; actual != expected {
+		t.Errorf("The response text is wrong. Actual: %s, Expected: %s", actual, expected)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	mockResp := `{"id":"123457890", "login":"testlogin", "issuer":"github", "token":"11111111", "name":null, "email":null, "isEmailConfirmed":false}`
+	w := httptest.NewRecorder()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(mockResp))
+	}))
+	defer server.Close()
+
+	os.Setenv("GITHUB_USER_INFO_URL", server.URL)
+
+	service := &api.Service{Client: server.Client(), BaseURL: server.URL}
+	req := httptest.NewRequest("GET", "/users/login", nil)
+	identity(w, req, service)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if actual, expected := resp.StatusCode, http.StatusOK; actual != expected {
+		t.Errorf("Status code is wrong. Actual: %d, Expected: %d", actual, expected)
+	}
+
+	text, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actual, expected := string(text), mockResp; actual != expected {
+		t.Errorf("The response text is wrong. Actual: %s, Expected: %s", actual, expected)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	mockResp := `{"id":"123457890"}`
+	mockIssuer := "testIssuer"
+	mockToken := "testToken"
+	mockLogin := "testlogin"
+	w := httptest.NewRecorder()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(mockResp))
+	}))
+	defer server.Close()
+
+	os.Setenv("GITHUB_USER_INFO_URL", server.URL)
+
+	data := map[string]string{
+		"issuer": mockIssuer,
+		"token":  mockToken,
+		"login":  mockLogin}
+	jsonValue, err := json.Marshal(data)
+
+	service := &api.Service{Client: server.Client(), BaseURL: server.URL}
+	req := httptest.NewRequest("POST", "/users", bytes.NewBuffer(jsonValue))
+	identity(w, req, service)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if actual, expected := resp.StatusCode, http.StatusOK; actual != expected {
+		t.Errorf("Status code is wrong. Actual: %d, Expected: %d", actual, expected)
+	}
+
+	text, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actual, expected := string(text), mockResp; actual != expected {
 		t.Errorf("The response text is wrong. Actual: %s, Expected: %s", actual, expected)
 	}
 }
