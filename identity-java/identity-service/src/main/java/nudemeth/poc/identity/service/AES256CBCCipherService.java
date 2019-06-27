efbffa1b0b19418fc36fae56db15e22e587edb64
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-06-27 21:43:46 +0700

    identity-java: Add concrete encryption service

---
+++ identity-java/identity-service/src/main/java/nudemeth/poc/identity/service/AES256CBCCipherService.java
@@ -0,0 +1,15 @@
+package nudemeth.poc.identity.service;
+
+public class AES256CBCCipherService implements CipherService {
+
+    @Override
+    public String encrypt(String message) {
+        return null;
+    }
+
+    @Override
+    public String decrypt(String message) {
+        return null;
+    }
+
+}
