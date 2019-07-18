4a17c5d6afcdbbfbed54aa389dbcb5c04da50719
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-07-18 22:47:04 +0700

    identity-java: Add token verifying method

--- identity-java/identity-service/src/main/java/nudemeth/poc/identity/service/JwtTokenService.java
+++ identity-java/identity-service/src/main/java/nudemeth/poc/identity/service/JwtTokenService.java
@@ -1,10 +1,15 @@
 package nudemeth.poc.identity.service;
 
+import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,30 +21,42 @@ import nudemeth.poc.identity.model.UserModel;
 public class JwtTokenService implements TokenService {
 
     private static final int TOKEN_LIFETIME_HOURS = 2;
-    private static final String DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
-    
-    private final CipherConfig config;
+    private static final DateTimeFormatter DATETIME_FORMAT = DateTimeFormatter.ISO_INSTANT;
+
+    private final Algorithm algorithm;
 
     @Autowired
     public JwtTokenService(CipherConfig config) {
-        this.config = config;
+        this.algorithm = Algorithm.HMAC256(config.getKey());
     }
 
     @Override
     public String create(UserModel model) {
-        Algorithm algorithm = Algorithm.HMAC256(config.getKey());
-        String exp = ZonedDateTime.now().plusHours(TOKEN_LIFETIME_HOURS).format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+        String expiryDateTime = ZonedDateTime.now().plusHours(TOKEN_LIFETIME_HOURS).format(DATETIME_FORMAT);
         String token = JWT.create()
             .withClaim("id", model.getId().toString())
             .withClaim("login", model.getLogin())
-            .withClaim("exp", exp)
+            .withClaim("exp", expiryDateTime)
             .sign(algorithm);
         return token;
     }
 
     @Override
     public boolean verify(String token) {
-        return false;
+        try {
+            JWTVerifier verifier = JWT.require(algorithm)
+            .build();
+            DecodedJWT jwt = verifier.verify(token);
+            String exp = jwt.getClaim("exp").asString();
+            ZonedDateTime expiryDateTime = Instant.parse(exp).atZone(ZoneId.systemDefault());
+            return isExpired(expiryDateTime);
+        } catch (JWTVerificationException ex) {
+            return false;
+        }
+    }
+
+    private boolean isExpired(ZonedDateTime expiryDateTime) {
+        return ZonedDateTime.now().compareTo(expiryDateTime) < 0;
     }
 
 }
