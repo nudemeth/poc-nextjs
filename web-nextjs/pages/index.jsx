0fad606f48b4a267d6d31b5e487e104d12c757dd
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-08-20 22:05:37 +0700

    web-nextjs: Add redirection when no access token

--- web-nextjs/pages/index.jsx
+++ web-nextjs/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Router from 'next/router'
 import { connect } from 'react-redux'
 import Container from '../components/layout/Container'
 import Catalog from '../components/page/index/Catalog'
@@ -11,12 +12,32 @@ class Index extends React.Component {
         super(props)
     }
 
-    static async getInitialProps({ ctx: { store } }) {
-        store.dispatch(loadItems())
+    static async getInitialProps({ ctx: { store, res, query } }) {
+        if (!this.isRedirect(query)) {
+            store.dispatch(loadItems())
+            return
+        }
+
+        this.redirect(res)
     }
 
     static propTypes = {
         dispatch: PropTypes.func.isRequired
+    }
+
+    static redirect(res) {
+        if (res) {
+            res.writeHead(302, {
+                Location: '/login'
+            })
+            res.end()
+        } else {
+            Router.push('/login')
+        }
+    }
+
+    static isRedirect(query) {
+        return !query.accessToken
     }
 
     componentDidMount() {
