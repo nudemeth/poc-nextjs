4ca7a2b459422996e5e6b586420966f4e00d0155
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-10-24 21:28:30 +0700

    web-nextjs: Update catalog to handle 401

--- web-nextjs/sagas/catalog.saga.js
+++ web-nextjs/sagas/catalog.saga.js
@@ -1,5 +1,6 @@
 import * as effects from 'redux-saga/effects'
 import * as actions from '../actions/catalog.actions'
+import { revokeAccessToken } from '../actions/identity.actions'
 import catalogApi from '../api/catalog.api'
 import * as catalogTypeSaga from './catalogType.saga'
 import * as catalogBrandSaga from './catalogBrand.saga'
@@ -9,10 +10,17 @@ function * loadItemsWorker() {
         const catalogTypes = yield effects.call(catalogTypeSaga.getSelectedCatalogTypeIds)
         const catalogBrands = yield effects.call(catalogBrandSaga.getSelectedCatalogBrandIds)
         const res = yield effects.call(catalogApi.getItems, catalogTypes, catalogBrands)
+        if (res.status !== 200) {
+            throw res.status
+        }
         const data = yield res.json()
         yield effects.put(actions.loadItemsSuccess(data))
     } catch(err) {
-        yield effects.put(actions.loadItemsFailure(err))
+        if (err === 401) {
+            yield effects.put(revokeAccessToken())
+        } else {
+            yield effects.put(actions.loadItemsFailure(err))
+        }
     }
 }
 
