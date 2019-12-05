ed9114f0a2fdd57411891242ef2e6759e15c866b
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-12-05 11:41:29 +0700

    ordering-scala: Refactor OrderQuery

--- ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/query/OrderQuery.scala
+++ ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/query/OrderQuery.scala
@@ -41,21 +41,17 @@ class OrderQuery extends OrderQueryable {
       .bind()
       .setUuid("buyerId", id)
 
-    session.executeAsync(query).toScala.map { r =>
-      r.currentPage().asScala
-    }.map { r =>
-      buildOrderSummary(r)
-    }
-  }
-
-  private def buildOrderSummary(rows: Iterable[Row]): Vector[OrderSummary] = {
-    rows.map { r =>
-      OrderSummary(
-        r.getUuid(CqlIdentifier.fromCql("order_id")),
-        r.getInstant(CqlIdentifier.fromCql("order_date")).atOffset(ZoneOffset.UTC),
-        r.getString(CqlIdentifier.fromCql("status_name")),
-        r.getInt(CqlIdentifier.fromCql("total")))
-    }.toVector
+    for {
+      result <- session.executeAsync(query).toScala
+    } yield for {
+      row <- result.currentPage().asScala.toVector
+      orderSummary = OrderSummary(
+        row.getUuid(CqlIdentifier.fromCql("order_id")),
+        row.getInstant(CqlIdentifier.fromCql("order_date")).atOffset(ZoneOffset.UTC),
+        row.getString(CqlIdentifier.fromCql("status_name")),
+        row.getInt(CqlIdentifier.fromCql("total"))
+      )
+    } yield orderSummary
   }
 
   override def getOrderAsync(id: UUID): Future[Option[Order]] = {
@@ -67,17 +63,11 @@ class OrderQuery extends OrderQueryable {
       .prepare(getCardTypesCql)
       .bind()
 
-    session.executeAsync(query).toScala.map { r =>
-      r.currentPage().asScala
-    }.map { r =>
-      buildCardTypes(r)
-    }
-  }
-
-  private def buildCardTypes(rows: Iterable[Row]): Vector[CardType] = {
-    rows.map { r =>
-      CardType(
-        r.getString(CqlIdentifier.fromCql("name")))
-    }.toVector
+    for {
+      result <- session.executeAsync(query).toScala
+    } yield for {
+      row <- result.currentPage().asScala.toVector
+      cardTypes = CardType(row.getString(CqlIdentifier.fromCql("name")))
+    } yield cardTypes
   }
 }
