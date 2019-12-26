99bd0f47285ccb4c13e87825b4679b4d531d756f
nudemeth <7686682+nudemeth@users.noreply.github.com>
2019-12-26 20:33:34 +0700

    ordering-scala: Update RequestManager

---
+++ ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/OrderingContext.scala
@@ -0,0 +1,5 @@
+package nudemeth.poc.ordering.infrastructure
+
+case class OrderingContext() {
+
+}
--- ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/idempotency/RequestManager.scala
+++ ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/idempotency/RequestManager.scala
@@ -1,9 +1,11 @@
 package nudemeth.poc.ordering.infrastructure.idempotency
 import java.util.UUID
 
+import nudemeth.poc.ordering.infrastructure.OrderingContext
+
 import scala.concurrent.Future
 
-case class RequestManager() extends RequestManagerOperations {
+case class RequestManager(context: OrderingContext) extends RequestManagerOperations {
   override def existAsync(id: UUID): Future[Boolean] = {
     Future.successful(true)
   }
