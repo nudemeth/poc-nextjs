58cfbcb4e03949c874489a43129061f30c252b89
nudemeth <7686682+nudemeth@users.noreply.github.com>
2020-01-07 21:40:30 +0700

    ordering-scala: Update CancelOrderCommandHandler

--- ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/command/CancelOrderCommandHandler.scala
+++ ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/command/CancelOrderCommandHandler.scala
@@ -1,11 +1,20 @@
 package nudemeth.poc.ordering.api.application.command
 
-import nudemeth.poc.ordering.api.infrastructure.mediator.{ MediatorDuty, RequestHandler }
+import nudemeth.poc.ordering.api.infrastructure.mediator.{MediatorDuty, RequestHandler}
+import nudemeth.poc.ordering.infrastructure.repository.OrderRepositoryOperations
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-case class CancelOrderCommandHandler() extends RequestHandler[CancelOrderCommand, Boolean] {
-  override def handle(request: CancelOrderCommand, mediator: MediatorDuty): Future[Boolean] = {
-    Future.successful(true)
+case class CancelOrderCommandHandler(orderRepository: OrderRepositoryOperations) extends RequestHandler[CancelOrderCommand, Boolean] {
+  override def handle(command: CancelOrderCommand, mediator: MediatorDuty): Future[Boolean] = {
+    val orderToUpdate = orderRepository.getOrderAsync(command.orderId)
+    orderToUpdate.flatMap { o =>
+      if (o.isEmpty) {
+        Future.successful(false)
+      } else {
+        val updatedOrder = o.get._1.setCancelledStatus()
+        orderRepository.addOrUpdateOrderAsync(updatedOrder, o.get._2).map(_ => true)
+      }
+    }
   }
 }
--- ordering-scala/ordering-domain/src/main/scala/nudemeth/poc/ordering/domain/model/aggregate/order/Order.scala
+++ ordering-scala/ordering-domain/src/main/scala/nudemeth/poc/ordering/domain/model/aggregate/order/Order.scala
@@ -3,6 +3,8 @@ package nudemeth.poc.ordering.domain.model.aggregate.order
 import java.time.Instant
 import java.util.UUID
 
+import nudemeth.poc.ordering.domain.exception.OrderingDomainException
+
 case class Order(
   orderId: UUID,
   buyerId: UUID,
@@ -11,5 +13,17 @@ case class Order(
   orderStatus: String,
   orderItems: Vector[OrderItem],
   description: Option[String]) {
+
+  def setCancelledStatus(): Order = {
+    if (orderStatus == "Paid" || orderStatus == "Shipped") {
+      raiseStatusChangeException("Cancelled")
+    }
+    this.copy(orderStatus = "Cancelled", description = Some("The order was shipped."))
+    //AddDomainEvent
+  }
+
+  private def raiseStatusChangeException(nextStatus: String): Unit = {
+    throw OrderingDomainException(s"Is not possible to change the order status from $orderStatus to $nextStatus.")
+  }
 
 }
