b218e37b1dcfb38c82be15fe94ba1e8581bfcff9
nudemeth <7686682+nudemeth@users.noreply.github.com>
2020-02-11 21:58:11 +0700

    ordering-scala: Update Mediator

--- ordering-scala/ordering-util/src/main/scala/nudemeth/poc/ordering/util/mediator/Mediator.scala
+++ ordering-scala/ordering-util/src/main/scala/nudemeth/poc/ordering/util/mediator/Mediator.scala
@@ -7,4 +7,8 @@ class Mediator(handlers: Map[Class[_ <: Request[Any]], _ <: RequestHandler[_ <: 
     val handler = handlers.find(h => h._1.equals(request.getClass)).getOrElse(throw new NoSuchElementException(classOf[Request[TResponse]].toString))
     handler.asInstanceOf[RequestHandler[Request[TResponse], TResponse]].handle(request, this)
   }
+
+  override def publish[TNotification <: Notification](notification: TNotification)(implicit executor: ExecutionContext): Future[Unit] = {
+    Future.unit
+  }
 }
--- ordering-scala/ordering-util/src/main/scala/nudemeth/poc/ordering/util/mediator/MediatorDuty.scala
+++ ordering-scala/ordering-util/src/main/scala/nudemeth/poc/ordering/util/mediator/MediatorDuty.scala
@@ -4,4 +4,5 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 trait MediatorDuty {
   def send[TResponse](request: Request[TResponse])(implicit executor: ExecutionContext): Future[TResponse]
+  def publish[TNotification <: Notification](notification: TNotification)(implicit executor: ExecutionContext): Future[Unit]
 }
