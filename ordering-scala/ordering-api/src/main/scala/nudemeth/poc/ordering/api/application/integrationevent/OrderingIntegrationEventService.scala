b9d1de8f5c4e8c141f90cec7b512cc53bcdb2771
nudemeth <7686682+nudemeth@users.noreply.github.com>
2020-02-15 13:21:36 +0700

    ordering-scala: Update OrderingIntegrationEventService

--- ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/integrationevent/OrderingIntegrationEventService.scala
+++ ordering-scala/ordering-api/src/main/scala/nudemeth/poc/ordering/api/application/integrationevent/OrderingIntegrationEventService.scala
@@ -1,11 +1,13 @@
 package nudemeth.poc.ordering.api.application.integrationevent
-import nudemeth.poc.ordering.infrastructure.eventbus.IntegrationEvent
+import nudemeth.poc.ordering.infrastructure.eventbus.{EventBusOperations, IntegrationEvent}
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
-case class OrderingIntegrationEventService() extends OrderingIntegrationEventServiceOperations {
+case class OrderingIntegrationEventService(eventBus: EventBusOperations) extends OrderingIntegrationEventServiceOperations {
   override def publishThroughEventBusAsync(event: IntegrationEvent)(implicit executor: ExecutionContext): Future[Unit] = {
-    saveEventAndOrderingContextChangesAsync(event)
+    saveEventAndOrderingContextChangesAsync(event).map {_ =>
+      eventBus.publish(event)
+    }
   }
 
   private def saveEventAndOrderingContextChangesAsync(event: IntegrationEvent): Future[Unit] = {
