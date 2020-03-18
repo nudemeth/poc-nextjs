612d7e3eca3697058441f0b65609f6334ad526b9
nudemeth <7686682+nudemeth@users.noreply.github.com>
2020-03-18 20:52:59 +0700

    ordering-scala: Update EventBusSubscriptionsManager

--- ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/eventbus/EventBusSubscriptionsManager.scala
+++ ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/eventbus/EventBusSubscriptionsManager.scala
@@ -1,6 +1,8 @@
 package nudemeth.poc.ordering.infrastructure.eventbus
 
+import scala.reflect.runtime.universe._
+
 trait EventBusSubscriptionsManager {
-  def addSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](): Map[String, Vector[SubscriptionInfo]]
-  def removeSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](): Map[String, Vector[SubscriptionInfo]]
+  def addSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](handlers: Map[String, Vector[SubscriptionInfo]])(implicit eventTag: TypeTag[T], handlerTag: TypeTag[TH]): Map[String, Vector[SubscriptionInfo]]
+  def removeSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](handlers: Map[String, Vector[SubscriptionInfo]])(implicit eventTag: TypeTag[T], handlerTag: TypeTag[TH]): Map[String, Vector[SubscriptionInfo]]
 }
--- ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/eventbus/InMemoryEventBusSubscriptionsManager.scala
+++ ordering-scala/ordering-infrastructure/src/main/scala/nudemeth/poc/ordering/infrastructure/eventbus/InMemoryEventBusSubscriptionsManager.scala
@@ -1,12 +1,14 @@
 package nudemeth.poc.ordering.infrastructure.eventbus
 
+import scala.reflect.runtime.universe._
+
 case class InMemoryEventBusSubscriptionsManager() extends EventBusSubscriptionsManager {
 
-  override def addSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](): Map[String, Vector[SubscriptionInfo]] = {
-    Map()
+  override def addSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](handlers: Map[String, Vector[SubscriptionInfo]])(implicit eventTag: TypeTag[T], handlerTag: TypeTag[TH]): Map[String, Vector[SubscriptionInfo]] = {
+    handlers + ("" -> Vector())
   }
 
-  override def removeSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](): Map[String, Vector[SubscriptionInfo]] = {
-    Map()
+  override def removeSubscription[T <: IntegrationEvent, TH <: IntegrationEventHandlerOperations[T]](handlers: Map[String, Vector[SubscriptionInfo]])(implicit eventTag: TypeTag[T], handlerTag: TypeTag[TH]): Map[String, Vector[SubscriptionInfo]] = {
+    handlers + ("" -> Vector())
   }
 }
